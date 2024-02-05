e93cc62b34f9aa17af2affd3caee66298f8beaf7
kpauner <kpauner@gmail.com>
2024-02-05 12:29:54 +0100

    add: header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,30 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
+import type { Metadata } from "next"
+import { Poppins } from "next/font/google"
+import "./globals.css"
+import { cn } from "@/lib/utils"
+import Header from "@/components/header"
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
+})
 
 export const metadata: Metadata = {
   title: "Fitness verden",
   description: "Dit online fitness community",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={cn("relative h-full antialiased", poppins.className)}>
-        <main className="relative flex flex-col min-h-screen">{children}</main>
+        <Header />
+        <main className="relative flex min-h-screen flex-col">{children}</main>
       </body>
     </html>
-  );
+  )
 }
---
+++ config/navigation.ts
@@ -0,0 +1,14 @@
+export const nav = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Search",
+    path: "/search",
+  },
+  {
+    name: "My Schedule",
+    path: "/myschedule",
+  },
+]
